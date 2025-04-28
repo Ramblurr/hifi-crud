c71ad63bf85dc5e7287a59410b0026568ef138c8
Casey Link <unnamedrambler@gmail.com>
2025-04-28 10:51:41 +0200

    Rename mis-named page-state -> tab-state

--- src/app/tab_state.clj
+++ src/app/tab_state.clj
@@ -5,9 +5,9 @@
    [clojure.core.async :as a]
    [chime.core :as chime]))
 
-(def !page-state (atom {}))
+(def !tab-state (atom {}))
 
-@!page-state
+@!tab-state
 
 (defn transact!
   "Helper function to be used by your command/action handlers.
@@ -16,10 +16,10 @@
   "
   [tab-id f]
   (if tab-id
-    (swap! !page-state update tab-id (fn [state]
-                                       (-> state
-                                           (f)
-                                           (assoc ::modified (System/currentTimeMillis)))))
+    (swap! !tab-state update tab-id (fn [state]
+                                      (-> state
+                                          (f)
+                                          (assoc ::modified (System/currentTimeMillis)))))
 
     (throw (ex-info "No tab-id in request" {}))))
 
@@ -28,16 +28,16 @@
   [<ch tab-id]
   (assert <ch "Channel must be provided")
   (assert tab-id "Tab ID must be provided")
-  (swap! !page-state assoc tab-id {::created (System/currentTimeMillis)})
-  (add-watch !page-state tab-id (fn [watch-key _ _ _]
-                                  (when-not (a/>!! <ch :state-changed)
-                                    (remove-watch !page-state watch-key)))))
+  (swap! !tab-state assoc tab-id {::created (System/currentTimeMillis)})
+  (add-watch !tab-state tab-id (fn [watch-key _ _ _]
+                                 (when-not (a/>!! <ch :state-changed)
+                                   (remove-watch !tab-state watch-key)))))
 
 (defn remove-tab-state!
   "Cleans up a tab state session"
   [tab-id]
-  (swap! !page-state dissoc tab-id)
-  (remove-watch !page-state tab-id))
+  (swap! !tab-state dissoc tab-id)
+  (remove-watch !tab-state tab-id))
 
 ;; Tab sessions older than this are pruned
 (def STALE-THRESHOLD-HOURS 12)
@@ -45,35 +45,35 @@
 (defn stale? [now created modified]
   (> (- now (or modified created)) (* STALE-THRESHOLD-HOURS 3600000)))
 
-(defn clean-stale-page-state
+(defn clean-stale-tab-state
   "Removes tab-ids that are stale, where stale is defined as not having been modified or created in the last 24 hours."
-  [page-state]
+  [tab-state]
   (let [now (System/currentTimeMillis)]
     (reduce-kv (fn [acc tab-id {:keys [::created ::modified]}]
                  (if (stale? now created modified)
                    (dissoc acc tab-id)
                    acc))
-               page-state
-               page-state)))
+               tab-state
+               tab-state)))
 
 (defn clean-stale-watches!
-  "Removes watches for tab-ids that are no longer in the page state."
+  "Removes watches for tab-ids that are no longer in the tab state."
   []
-  (let [watches       (-> !page-state .getWatches keys)
-        stale-watches (remove #(get @!page-state %) watches)]
+  (let [watches       (-> !tab-state .getWatches keys)
+        stale-watches (remove #(get @!tab-state %) watches)]
     (doseq [watch-key stale-watches]
-      (remove-watch !page-state watch-key))))
+      (remove-watch !tab-state watch-key))))
 
-(defn start-clean-page-state-job
-  "Starts a job that cleans stale page state every 10 seconds."
+(defn start-clean-tab-state-job
+  "Starts a job that cleans stale tab state every 10 seconds."
   []
   (chime/chime-at (chime/periodic-seq (Instant/now) (Duration/ofSeconds 60))
                   (fn [_]
-                    (swap! !page-state clean-stale-page-state)
+                    (swap! !tab-state clean-stale-tab-state)
                     (clean-stale-watches!))))
 
 (defn ctx-start [ctx]
-  (assoc ctx :clean-tab-state-chime (start-clean-page-state-job)))
+  (assoc ctx :clean-tab-state-chime (start-clean-tab-state-job)))
 
 (defn ctx-stop [ctx]
   (when-let [chime (:clean-tab-state-chime ctx)]
@@ -83,4 +83,4 @@
   (h/new-uid))
 
 (defn tab-state! [tab-id]
-  (get @!page-state tab-id))
+  (get @!tab-state tab-id))
