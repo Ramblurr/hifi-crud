a67ea39b843b378efbf3656095caa631134c5287
Casey Link <unnamedrambler@gmail.com>
2025-07-12 22:36:38 +0200

    hifi-util: improve docs, improve consistency of fn names and signatures

--- libs/hifi-util/src/hifi/util/crypto.clj
+++ libs/hifi-util/src/hifi/util/crypto.clj
@@ -1,11 +1,24 @@
 ;; Copyright © 2025 Casey Link <casey@outskirtslabs.com>
 ;; SPDX-License-Identifier: EUPL-1.2
 (ns hifi.util.crypto
-  "Utilities related to security"
+  "Cryptographic utilities for secure hashing and random value generation.
+
+   Features:
+   - Secure random bytes/strings with cryptographic randomness
+   - SHA-256/384 hashing with multiple input types
+   - HMAC-SHA256 for message authentication
+   - SRI (Subresource Integrity) hash generation per W3C spec
+   - Timing-attack resistant equality comparison
+
+   Naming conventions:
+   - Base functions (sha256, sha384) operate on byte arrays
+   - -stream suffix for input streams
+   - -file suffix for filesystem files
+   - -resource suffix for classpath resources
+   - -hex suffix for hex string output"
   (:refer-clojure :exclude [bytes])
   (:require
    [clojure.java.io :as io]
-   [clojure.string :as str]
    [crypto.equality :as equality]
    [hifi.util.codec :as codec])
   (:import
@@ -17,81 +30,100 @@
   (SecureRandom/new))
 
 (defn bytes
-  "Returns a random byte array of the specified size."
-  ^byte/1 [size]
+  "Returns secure random bytes of specified size."
+  ^bytes [size]
   (let [seed (byte-array size)]
     (.nextBytes secure-random seed)
     seed))
 
-(defn rand-string [n]
+(defn rand-string
+  "Returns base64-encoded random string of n bytes."
+  [n]
   (codec/->base64 (bytes n)))
 
 (defn random-unguessable-uid
-  "Returns a URL-safe base64-encoded 160-bit (20 byte) random value.
+  "Returns URL-safe base64-encoded 160-bit (20 byte) random value.
 
-  Speed is similar random-uuid. See https://neilmadden.blog/2018/08/30/moving-away-from-uuids/
-  for an article (from 2018!) on why UUIDs should not be used in cases where cryptographically secure
-  randomness is required."
+   Prefer over UUIDs for cryptographically secure randomness.
+   See: https://neilmadden.blog/2018/08/30/moving-away-from-uuids/"
   []
   (codec/->base64 (bytes 20)))
 
 (def new-uid
-  "Allows uid implementation to be changed if need be."
+  "Alias for random-unguessable-uid."
   random-unguessable-uid)
 
 (def eq?
-  "Test whether two sequences of characters or bytes are equal in a way that
-  protects against timing attacks. Note that this does not prevent an attacker
-  from discovering the *length* of the data being compared."
+  "Timing-attack resistant equality comparison for strings or bytes.
+   Note: Does not hide length information."
   equality/eq?)
 
-(defn secret-key->hmac-sha256-keyspec [secret-key]
-  (SecretKeySpec/new (String/.getBytes secret-key) "HmacSHA256"))
+(defn secret-key->hmac-sha256-keyspec
+  "Converts string key to HMAC-SHA256 key specification."
+  [^String secret-key]
+  (SecretKeySpec. (.getBytes secret-key) "HmacSHA256"))
 
 (defn hmac-sha256
-  "Used for quick stateless csrf token generation."
-  [key-spec data]
+  "Computes HMAC-SHA256 and returns base64-encoded result."
+  [key-spec ^String data]
   (-> (doto (Mac/getInstance "HmacSHA256")
         (.init key-spec))
-      (.doFinal (String/.getBytes data))
+      (.doFinal (.getBytes data))
       codec/->base64))
 
-(defn hash-stream-data ^MessageDigest
-  [^String algo ^java.io.InputStream in]
+(defn hash-stream-data
+  "Returns MessageDigest after hashing stream with algorithm."
+  ^MessageDigest [^String algo ^java.io.InputStream in]
   (when in
     (let [d (MessageDigest/getInstance algo)
           buffer (byte-array 5120)]
       (loop []
         (let [read-size (.read in buffer 0 5120)]
           (when-not (= read-size -1)
-            (.update d ^bytes buffer 0 read-size)
+            (.update d buffer 0 read-size)
             (recur))))
       d)))
 
+(defn hash-bytes
+  "Hashes byte array with algorithm."
+  ^bytes [^String algo ^bytes input-bytes]
+  (.digest (hash-stream-data algo (io/input-stream input-bytes))))
+
+(defn hash-file
+  "Hashes file with algorithm. Returns nil if file doesn't exist."
+  ^bytes [^String algo file-path]
+  (let [file (io/file file-path)]
+    (when (.exists file)
+      (with-open [in (io/input-stream file)]
+        (.digest (hash-stream-data algo in))))))
+
+(defn hash-resource
+  "Hashes classpath resource with algorithm."
+  ^bytes [^String algo path]
+  (if-let [resource (io/resource path)]
+    (with-open [in (io/input-stream resource)]
+      (.digest (hash-stream-data algo in)))
+    (throw (ex-info "Cannot load resource from classpath" {:path path}))))
+
 (defn sha384
+  "Computes SHA-384 hash of byte array."
   ^bytes [^bytes input-bytes]
-  (-> (doto (MessageDigest/getInstance "SHA-384")
-        (.update input-bytes))
-      (.digest)))
+  (hash-bytes "SHA-384" input-bytes))
 
 (defn sha384-stream
-  ^bytes [^bytes ^java.io.InputStream in]
+  "Computes SHA-384 hash of input stream."
+  ^bytes [^java.io.InputStream in]
   (.digest (hash-stream-data "SHA-384" in)))
 
 (defn sha384-resource
-  "Computes SHA-384 hash of a classpath resource and returns bytes."
+  "Computes SHA-384 hash of classpath resource."
   ^bytes [path]
-  (if-let [resource (io/resource path)]
-    (with-open [in (io/input-stream resource)]
-      (sha384-stream in))
-    (throw (ex-info "Cannot load resource from classpath" {:path path}))))
+  (hash-resource "SHA-384" path))
 
 (defn sha256
   "Computes SHA-256 hash of byte array."
   ^bytes [^bytes input-bytes]
-  (-> (doto (MessageDigest/getInstance "SHA-256")
-        (.update input-bytes))
-      (.digest)))
+  (hash-bytes "SHA-256" input-bytes))
 
 (defn sha256-stream
   "Computes SHA-256 hash of input stream."
@@ -99,53 +131,60 @@
   (.digest (hash-stream-data "SHA-256" in)))
 
 (defn sha256-file
-  "Computes SHA-256 hash of a file and returns bytes."
+  "Computes SHA-256 hash of file, returns nil if file doesn't exist."
   ^bytes [file-path]
-  (let [file (io/file file-path)]
-    (when (.exists file)
-      (with-open [in (io/input-stream file)]
-        (sha256-stream in)))))
+  (hash-file "SHA-256" file-path))
 
 (defn sha256-file-hex
-  "Computes SHA-256 hash of a file and returns hex string."
+  "Computes SHA-256 hash of file and returns hex string."
   [file-path]
   (when-let [hash-bytes (sha256-file file-path)]
     (codec/->hex hash-bytes)))
 
 (defn sha384-file
-  "Computes SHA-384 hash of a file and returns bytes."
+  "Computes SHA-384 hash of file, returns nil if file doesn't exist."
   ^bytes [file-path]
-  (let [file (io/file file-path)]
-    (when (.exists file)
-      (with-open [in (io/input-stream file)]
-        (sha384-stream in)))))
+  (hash-file "SHA-384" file-path))
 
 (defn sha256-resource
-  "Computes SHA-256 hash of a classpath resource and returns bytes."
+  "Computes SHA-256 hash of classpath resource."
   ^bytes [path]
-  (if-let [resource (io/resource path)]
-    (with-open [in (io/input-stream resource)]
-      (sha256-stream in))
-    (throw (ex-info "Cannot load resource from classpath" {:path path}))))
+  (hash-resource "SHA-256" path))
 
 (defn sha256-resource-hex
-  "Computes SHA-256 hash of a classpath resource and returns hex string."
+  "Computes SHA-256 hash of classpath resource and returns hex string."
   [path]
   (codec/->hex (sha256-resource path)))
 
+(def ^:private sri-algorithm-map
+  "Maps W3C SRI algorithm tokens to Java MessageDigest names.
+   See: https://w3c.github.io/webappsec-subresource-integrity/#terms
+
+   \"The valid SRI hash algorithm token set is the ordered set
+   « \"sha256\", \"sha384\", \"sha512\" » (corresponding to SHA-256,
+   SHA-384, and SHA-512 respectively).\""
+  {"sha256" "SHA-256"
+   "sha384" "SHA-384"
+   "sha512" "SHA-512"})
+
 (defn sri-hash-stream
-  "Computes SRI hash for a stream with the given algorithm.
-   Returns the full SRI string (e.g., 'sha384-...')."
+  "Computes SRI hash for stream with given algorithm.
+   Returns SRI string format: '{algo}-{base64-hash}'.
+
+   Only accepts algorithms defined in the W3C SRI spec:
+   sha256, sha384, sha512 (exact lowercase names).
+   See: https://w3c.github.io/webappsec-subresource-integrity/"
   [algo ^java.io.InputStream in]
-  (let [digest-bytes (.digest (hash-stream-data algo in))
-        ;; Convert algorithm name to SRI format (e.g., "SHA-384" -> "sha384")
-        sri-algo (-> algo
-                     str/lower-case
-                     (str/replace "-" ""))]
-    (str sri-algo "-" (codec/->base64 digest-bytes))))
+  (if-let [java-algo (get sri-algorithm-map algo)]
+    (let [digest-bytes (.digest (hash-stream-data java-algo in))]
+      (str algo "-" (codec/->base64 digest-bytes)))
+    (throw (ex-info "Invalid SRI algorithm"
+                    {:algorithm algo
+                     :valid-algorithms (set (keys sri-algorithm-map))}))))
 
 (defn sri-hash-file
-  "Computes SRI hash of a file for the given algorithm."
+  "Computes SRI hash of file for given algorithm.
+   Only accepts W3C SRI spec algorithms: sha256, sha384, sha512."
   [algo file-path]
   (let [file (io/file file-path)]
     (when (.exists file)
@@ -153,7 +192,8 @@
         (sri-hash-stream algo in)))))
 
 (defn sri-hash-resource
-  "Computes SRI hash of a classpath resource for the given algorithm."
+  "Computes SRI hash of classpath resource for given algorithm.
+   Only accepts W3C SRI spec algorithms: sha256, sha384, sha512."
   [algo path]
   (if-let [resource (io/resource path)]
     (with-open [in (io/input-stream resource)]
@@ -161,12 +201,11 @@
     (throw (ex-info "Cannot load resource from classpath" {:path path}))))
 
 (defn sri-sha384-file
-  "Computes SHA-384 hash of a file and returns base64 string for SRI."
+  "Computes SHA-384 SRI hash of file."
   [file-path]
-  (sri-hash-file "SHA-384" file-path))
+  (sri-hash-file "sha384" file-path))
 
 (defn sri-sha384-resource
-  "Computes SHA-384 hash of a classpath resource and returns SRI string."
+  "Computes SHA-384 SRI hash of classpath resource."
   [path]
-  (sri-hash-resource "SHA-384" path))
-
+  (sri-hash-resource "sha384" path))
--- libs/hifi-util/test/hifi/util/crypto_test.clj
+++ libs/hifi-util/test/hifi/util/crypto_test.clj
@@ -1,134 +1,109 @@
 ;; Copyright © 2025 Casey Link <casey@outskirtslabs.com>
-;; SPDX-License-Identifier: EUPL-1.2 
+;; SPDX-License-Identifier: EUPL-1.2
 
- (ns hifi.util.crypto-test
-   (:require
-    [clojure.test :refer [deftest testing is are]]
-    [clojure.java.io :as io]
-    [clojure.string :as str]
-    [hifi.util.crypto :as crypto]
-    [hifi.util.codec :as codec]))
+(ns hifi.util.crypto-test
+  (:require
+   [clojure.test :refer [deftest testing is are]]
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [hifi.util.crypto :as crypto]
+   [hifi.util.codec :as codec]))
 
-(deftest sha256-test
-  (testing "computes SHA-256 hash correctly"
-    ;; Test vector from https://www.di-mgt.com.au/sha_testvectors.html
-    (let [hash-bytes (crypto/sha256 (.getBytes "abc"))
-          hash-hex (codec/->hex hash-bytes)]
-      (is (= "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad" hash-hex))))
+(deftest hash-bytes-test
+  (testing "hash functions compute correct values"
+    (are [hash-fn expected-hex input]
+         (= expected-hex (codec/->hex (hash-fn (.getBytes input))))
+      ;; Test vectors from https://www.di-mgt.com.au/sha_testvectors.html
+      crypto/sha256 "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad" "abc"
+      crypto/sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" ""
+      crypto/sha384 "cb00753f45a35e8bb5a03d699ac65007272c32ab0eded1631a8b605a43ff5bed8086072ba1e7cc2358baeca134c825a7" "abc")))
 
-  (testing "handles empty input"
-    (let [hash-bytes (crypto/sha256 (.getBytes ""))
-          hash-hex (codec/->hex hash-bytes)]
-      (is (= "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" hash-hex)))))
-
-(deftest sha384-test
-  (testing "computes SHA-384 hash correctly"
-    ;; Test vector
-    (let [hash-bytes (crypto/sha384 (.getBytes "abc"))
-          hash-hex (codec/->hex hash-bytes)]
-      (is (= "cb00753f45a35e8bb5a03d699ac65007272c32ab0eded1631a8b605a43ff5bed8086072ba1e7cc2358baeca134c825a7" hash-hex)))))
-
-(deftest sha256-file-test
-  (testing "computes SHA-256 hash of a file returning bytes"
-    (let [temp-file (java.io.File/createTempFile "test" ".txt")]
+(deftest hash-file-test
+  (testing "file hash functions"
+    (let [temp-file (java.io.File/createTempFile "test" ".txt")
+          test-content "hello world"
+          sha256-expected "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"]
       (try
-        (spit temp-file "hello world")
-        (let [hash-bytes (crypto/sha256-file (.getAbsolutePath temp-file))]
-          (is (bytes? hash-bytes))
-          (is (= 32 (count hash-bytes))) ;; SHA-256 is 32 bytes
-          (is (= "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
-                 (codec/->hex hash-bytes))))
+        (spit temp-file test-content)
+        (testing "compute correct hash values"
+          (are [hash-fn byte-count] (let [result (hash-fn (.getAbsolutePath temp-file))]
+                                      (and (bytes? result)
+                                           (= byte-count (count result))))
+            crypto/sha256-file 32
+            crypto/sha384-file 48)
+
+          (is (= sha256-expected (crypto/sha256-file-hex (.getAbsolutePath temp-file)))))
+
         (finally
-          (.delete temp-file)))))
+          (.delete temp-file))))
 
-  (testing "returns nil for non-existent file"
-    (is (nil? (crypto/sha256-file "/non/existent/file.txt")))))
+    (testing "return nil for non-existent files"
+      (are [hash-fn] (nil? (hash-fn "/non/existent/file.txt"))
+        crypto/sha256-file
+        crypto/sha256-file-hex
+        crypto/sha384-file))))
 
-(deftest sha256-file-hex-test
-  (testing "computes SHA-256 hash of a file returning hex string"
-    (let [temp-file (java.io.File/createTempFile "test" ".txt")]
-      (try
-        (spit temp-file "hello world")
-        (is (= "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
-               (crypto/sha256-file-hex (.getAbsolutePath temp-file))))
-        (finally
-          (.delete temp-file)))))
+(deftest hash-resource-test
+  (testing "resource hash functions"
+    (let [test-resource "hifi/util/crypto_test.clj"]
+      (testing "compute hashes for existing resources"
+        (are [hash-fn byte-count] (let [result (hash-fn test-resource)]
+                                    (and (bytes? result)
+                                         (= byte-count (count result))))
+          crypto/sha256-resource 32
+          crypto/sha384-resource 48)
 
-  (testing "returns nil for non-existent file"
-    (is (nil? (crypto/sha256-file-hex "/non/existent/file.txt")))))
+        (let [hex-hash (crypto/sha256-resource-hex test-resource)]
+          (is (string? hex-hash))
+          (is (= 64 (count hex-hash)))
+          (is (re-matches #"[0-9a-f]{64}" hex-hash)))))
 
-(deftest sha384-file-test
-  (testing "computes SHA-384 hash of a file returning bytes"
-    (let [temp-file (java.io.File/createTempFile "test" ".txt")]
-      (try
-        (spit temp-file "hello world")
-        (let [hash-bytes (crypto/sha384-file (.getAbsolutePath temp-file))]
-          (is (bytes? hash-bytes))
-          (is (= 48 (count hash-bytes)))) ;; SHA-384 is 48 bytes
-        (finally
-          (.delete temp-file)))))
+    (testing "throw for non-existent resources"
+      (are [hash-fn] (thrown? clojure.lang.ExceptionInfo
+                              (hash-fn "non/existent/resource.txt"))
+        crypto/sha256-resource
+        crypto/sha256-resource-hex
+        crypto/sha384-resource))))
 
-  (testing "returns nil for non-existent file"
-    (is (nil? (crypto/sha384-file "/non/existent/file.txt")))))
+(deftest sri-hash-test
+  (testing "SRI hash generation"
+    (testing "generates correct format"
+      (let [test-bytes (.getBytes "test")]
+        (are [algo expected-prefix]
+             (let [sri (crypto/sri-hash-stream algo (io/input-stream test-bytes))]
+               (and (str/starts-with? sri expected-prefix)
+                    (re-matches (re-pattern (str expected-prefix "[A-Za-z0-9+/_-]+")) sri)))
+          "sha256" "sha256-"
+          "sha384" "sha384-"
+          "sha512" "sha512-")))
 
-(deftest sri-sha384-file-test
-  (testing "computes SHA-384 SRI hash of a file"
-    (let [temp-file (java.io.File/createTempFile "test" ".txt")]
-      (try
-        (spit temp-file "hello world")
-        (let [sri-hash (crypto/sri-sha384-file (.getAbsolutePath temp-file))]
-          (is (str/starts-with? sri-hash "sha384-"))
-          ;; The base64 part should be valid
-          (is (re-matches #"sha384-[A-Za-z0-9+/=_-]+" sri-hash)))
-        (finally
-          (.delete temp-file))))))
+    (testing "validates algorithm"
+      (is (thrown-with-msg? clojure.lang.ExceptionInfo
+                            #"Invalid SRI algorithm"
+                            (crypto/sri-hash-stream "MD5" (io/input-stream (.getBytes "test"))))))
 
-(deftest sha256-resource-test
-  (testing "computes SHA-256 hash of a classpath resource returning bytes"
-    ;; We need a known resource in the classpath - let's use this test file itself
-    (let [hash-bytes (crypto/sha256-resource "hifi/util/crypto_test.clj")]
-      (is (bytes? hash-bytes))
-      (is (= 32 (count hash-bytes))))) ;; SHA-256 is 32 bytes
+    (testing "works with files"
+      (let [temp-file (java.io.File/createTempFile "test" ".txt")]
+        (try
+          (spit temp-file "test content")
+          (are [algo expected-prefix]
+               (str/starts-with? (crypto/sri-hash-file algo (.getAbsolutePath temp-file))
+                                 expected-prefix)
+            "sha256" "sha256-"
+            "sha384" "sha384-"
+            "sha512" "sha512-")
 
-  (testing "throws for non-existent resource"
-    (is (thrown? clojure.lang.ExceptionInfo
-                 (crypto/sha256-resource "non/existent/resource.txt")))))
+          (let [sri (crypto/sri-sha384-file (.getAbsolutePath temp-file))]
+            (is (str/starts-with? sri "sha384-"))
+            (is (re-matches #"sha384-[A-Za-z0-9+/=_-]+" sri)))
 
-(deftest sha256-resource-hex-test
-  (testing "computes SHA-256 hash of a classpath resource returning hex string"
-    ;; We need a known resource in the classpath - let's use this test file itself
-    (let [hash (crypto/sha256-resource-hex "hifi/util/crypto_test.clj")]
-      (is (string? hash))
-      (is (= 64 (count hash))) ;; SHA-256 is 32 bytes = 64 hex chars
-      (is (re-matches #"[0-9a-f]{64}" hash))))
+          (finally
+            (.delete temp-file)))))
 
-  (testing "throws for non-existent resource"
-    (is (thrown? clojure.lang.ExceptionInfo
-                 (crypto/sha256-resource-hex "non/existent/resource.txt")))))
-
-(deftest sri-hash-stream-test
-  (testing "generates correct SRI hash format"
-    (let [input-stream (io/input-stream (.getBytes "test"))]
-      (let [sri (crypto/sri-hash-stream "SHA-256" input-stream)]
-        (is (str/starts-with? sri "sha256-"))
-        (is (re-matches #"sha256-[A-Za-z0-9+/_-]+" sri))))))
-
-(deftest sri-hash-file-test
-  (testing "generates SRI hash for different algorithms"
-    (let [temp-file (java.io.File/createTempFile "test" ".txt")]
-      (try
-        (spit temp-file "test content")
-        (testing "SHA-256"
-          (let [sri (crypto/sri-hash-file "SHA-256" (.getAbsolutePath temp-file))]
-            (is (str/starts-with? sri "sha256-"))))
-        (testing "SHA-384"
-          (let [sri (crypto/sri-hash-file "SHA-384" (.getAbsolutePath temp-file))]
-            (is (str/starts-with? sri "sha384-"))))
-        (testing "SHA-512"
-          (let [sri (crypto/sri-hash-file "SHA-512" (.getAbsolutePath temp-file))]
-            (is (str/starts-with? sri "sha512-"))))
-        (finally
-          (.delete temp-file))))))
+    (testing "works with resources"
+      (let [sri (crypto/sri-sha384-resource "hifi/util/crypto_test.clj")]
+        (is (str/starts-with? sri "sha384-"))
+        (is (re-matches #"sha384-[A-Za-z0-9+/=_-]+" sri))))))
 
 (deftest hash-consistency-test
   (testing "file and stream functions produce same results"
@@ -136,9 +111,30 @@
           content "consistency test content"]
       (try
         (spit temp-file content)
-        (let [file-sha256 (crypto/sha256-file-hex (.getAbsolutePath temp-file))
-              stream-sha256 (with-open [in (io/input-stream temp-file)]
-                              (codec/->hex (crypto/sha256-stream in)))]
-          (is (= file-sha256 stream-sha256)))
+        (let [file-hash (crypto/sha256-file-hex (.getAbsolutePath temp-file))
+              stream-hash (with-open [in (io/input-stream temp-file)]
+                            (codec/->hex (crypto/sha256-stream in)))]
+          (is (= file-hash stream-hash)))
         (finally
           (.delete temp-file))))))
+
+(deftest random-generation-test
+  (testing "random value generation"
+    (testing "bytes function"
+      (are [size] (let [result (crypto/bytes size)]
+                    (and (bytes? result)
+                         (= size (count result))))
+        16 20 32))))
+
+(deftest hmac-test
+  (testing "HMAC-SHA256"
+    (let [key-spec (crypto/secret-key->hmac-sha256-keyspec "secret-key")
+          data     "test data"
+          hmac     (crypto/hmac-sha256 key-spec data)]
+      (is (string? hmac))
+      (is (re-matches #"[A-Za-z0-9+/=_-]+" hmac)))))
+
+(deftest timing-safe-equality-test
+  (testing "eq? function"
+    (is (crypto/eq? "is this thing on?" "is this thing on?"))
+    (is (not (crypto/eq? "a" "b")))))
