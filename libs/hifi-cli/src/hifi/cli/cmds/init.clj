320605fe4b2bc45f544b6da686b70d495a3a5bf1
Casey Link <casey@outskirtslabs.com>
2025-09-26 13:28:37 +0200

    cli: more scaffolding

--- libs/hifi-cli/src/hifi/cli/cmds/init.clj
+++ libs/hifi-cli/src/hifi/cli/cmds/init.clj
@@ -1,7 +1,6 @@
 (ns hifi.cli.cmds.init
   (:require
-   [hifi.cli.cmds.shared :as shared]
-   [babashka.cli :as cli]))
+   [hifi.cli.cmds.shared :as shared]))
 
 (declare spec)
 
@@ -9,18 +8,16 @@
 
 (defn handler
   [{:keys [opts args] :as i}]
-  (if (:help opts)
-    ((shared/help-printer spec) i)
-    (let [config (shared/load-config opts)]
-      (println config))))
+  (let [config (shared/load-config opts)]
+    (println config)))
 
 (def spec
-  {:spec (shared/with-shared-specs [:help :config-file] {})
-   :args [{:desc    "The name of the project, must be a qualified project name like com.example/my-app"
-           :ref     "<project-name>"}]
-   :args->opts [:project-name]
-   :examples examples
-   :description "Create a new hifi project in the current directory"
-   :doc "Extra docs"
-   :cmds ["init"]
-   :fn handler})
+  (shared/with-help handler
+    {:spec (shared/with-shared-specs [:help :config-file] {})
+     :args [{:desc    "The name of the project, must be a qualified project name like com.example/my-app"
+             :ref     "<project-name>"}]
+     :args->opts [:project-name]
+     :examples examples
+     :description "Create a new hifi project in the current directory"
+     :doc "Extra docs"
+     :cmds ["init"]}))
