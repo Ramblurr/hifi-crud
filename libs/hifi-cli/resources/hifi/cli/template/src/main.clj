d45ac0396e9c105af7aa4a8f27ec044d561cfdba
Casey Link <casey@outskirtslabs.com>
2025-10-09 18:25:27 +0200

    cli template: progress

--- libs/hifi-cli/resources/hifi/cli/template/src/main.clj
+++ libs/hifi-cli/resources/hifi/cli/template/src/main.clj
@@ -1,6 +1,36 @@
-(ns {{top/ns}}.{{main/ns}})
+(ns {{top/ns}}.{{main/ns}}
+  (:require
+   [hifi.http :as http]
+   [hifi.config :as config]
+   [hifi.assets :as assets]
+   [hifi.html :as html]
+   [hifi.core :as h]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (prn x "Hello, World!"))
+(def hypermedia-chain
+  [:parse-raw-params
+   :reverse-route
+   :exception
+   :parse-multipart
+   :session-cookie
+   :csrf-protection
+   :security-headers])
+
+(defn hello-world [req]
+  {:status 200
+   :headers {"content-type" "text/html"}
+   :body (:html (html/render req
+                             [html/doctype
+                              [:head
+                               [::html/stylesheet-link {:href "css/hello.css"}]]
+                              [:body
+                               [:h1 "Hello w World"]]]
+                             nil))})
+
+(h/defroutes routes
+  ["" {:middleware hypermedia-chain}
+   ["/" {:get  {:handler #'hello-world}}]
+   ["/error" {:get  {:handler (fn [_] (throw (ex-info "Uhoh" {})))}}]])
+
+(h/defplugin app
+  "My application"
+  {:hifi/routes (http/route-group routes)})
